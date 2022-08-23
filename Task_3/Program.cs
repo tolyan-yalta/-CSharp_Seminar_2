f1ce8f8d8d73497578543c83e49b9e2037212879
anatoly <tolyan.yalta@yandex.ru>
2022-08-23 14:59:57 +0300

    Created folder Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Программа, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+int number;
+Console.WriteLine("Введите цифру, обозначающую день недели: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+switch(number)
+{
+    case 1:
+        Console.WriteLine("Нет, это не выходной");
+        break;
+    case 2:
+        Console.WriteLine("Нет, это не выходной");
+        break;
+    case 3:
+        Console.WriteLine("Нет, это не выходной");
+        break;
+    case 4:
+        Console.WriteLine("Нет, это не выходной");
+        break;
+    case 5:
+        Console.WriteLine("Нет, это не выходной");
+        break;
+    case 6:
+        Console.WriteLine("Да, это выходной");
+        break;
+    case 7:
+        Console.WriteLine("Да, это выходной");
+        break;
+}
