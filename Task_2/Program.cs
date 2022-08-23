1992bfc996a73670882c18921c126b5bdea79f33
anatoly <tolyan.yalta@yandex.ru>
2022-08-23 14:45:07 +0300

    Created folder Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Программа, которая выводит третью цифру случайно сгенерированного числа (от 1 до 65536)
+// или сообщает, что третьей цифры нет.
+
+int number = new Random().Next(1, 65537);
+
+if(number < 100)
+{
+   Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    if(number < 1000)
+    {
+        int digit = number % 10;
+        Console.WriteLine("Сгенерировано число " + number + ", третья цифра " + digit);
+    }
+    else
+    {
+        if(number < 10000)
+        {
+            int temp = number % 100;
+            int digit = temp / 10;
+            Console.WriteLine("Сгенерировано число " + number + ", третья цифра " + digit);
+        }
+        else
+        {
+            int temp = number % 1000;
+            int digit = temp / 100;
+            Console.WriteLine("Сгенерировано число " + number + ", третья цифра " + digit);
+        }
+    }
+}
