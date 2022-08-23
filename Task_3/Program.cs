4f27918f8be06f1e9b92e380be70e4a046e2e1f9
anatoly <tolyan.yalta@yandex.ru>
2022-08-23 15:42:56 +0300

    Changed folder Task_3

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -8,24 +8,17 @@ number = Convert.ToInt32(Console.ReadLine());
 switch(number)
 {
     case 1:
-        Console.WriteLine("Нет, это не выходной");
-        break;
     case 2:
-        Console.WriteLine("Нет, это не выходной");
-        break;
     case 3:
-        Console.WriteLine("Нет, это не выходной");
-        break;
     case 4:
-        Console.WriteLine("Нет, это не выходной");
-        break;
     case 5:
         Console.WriteLine("Нет, это не выходной");
         break;
     case 6:
-        Console.WriteLine("Да, это выходной");
-        break;
     case 7:
         Console.WriteLine("Да, это выходной");
+        break;
+    default:
+        Console.WriteLine("Нет такого дня недели");
         break;
 }
